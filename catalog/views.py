a2e1b782291df22265404cd3b3a9009ccc84eb2d
stacy <nastia.2.r.2@gmail.com>
2019-10-19 19:33:22 +0300

    User authentication and permissions

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 5
+    context_object_name = 'book_list'  # your own name for the list as a template variable
+    queryset = Book.objects.all()[:5]  # Get 5 books containing the title war
+    template_name = '/books/book_list.html'
 
 
 def index(request):
@@ -16,13 +29,30 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+
 
--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -40,6 +40,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'locallibrary.urls'
 
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
