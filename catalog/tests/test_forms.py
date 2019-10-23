95875ab42feeb7366eb959dfd7c7f2d1cb29391a
stacy <nastia.2.r.2@gmail.com>
2019-10-23 05:53:37 +0300

    Form for books added

--- catalog/tests/test_forms.py
+++ catalog/tests/test_forms.py
@@ -17,65 +17,73 @@ class AuthorListViewTest(TestCase):
 
         for author_id in range(number_of_authors):
             Author.objects.create(
-                first_name=f'Christian {author_id}',
-                last_name=f'Surname {author_id}',
+                first_name=f"Christian {author_id}", last_name=f"Surname {author_id}"
             )
 
     def test_view_url_exists_at_desired_location(self):
-        response = self.client.get('/catalog/authors/')
+        response = self.client.get("/catalog/authors/")
         self.assertEqual(response.status_code, 200)
 
     def test_view_url_accessible_by_name(self):
-        response = self.client.get(reverse('authors'))
+        response = self.client.get(reverse("authors"))
         self.assertEqual(response.status_code, 200)
 
     def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('authors'))
+        response = self.client.get(reverse("authors"))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'catalog/author_list.html')
+        self.assertTemplateUsed(response, "catalog/author_list.html")
 
     def test_pagination_is_ten(self):
-        response = self.client.get(reverse('authors'))
+        response = self.client.get(reverse("authors"))
         self.assertEqual(response.status_code, 200)
-        self.assertTrue('is_paginated' in response.context)
-        self.assertTrue(response.context['is_paginated'] == True)
-        self.assertTrue(len(response.context['author_list']) == 10)
+        self.assertTrue("is_paginated" in response.context)
+        self.assertTrue(response.context["is_paginated"] == True)
+        self.assertTrue(len(response.context["author_list"]) == 10)
 
     def test_lists_all_authors(self):
         # Get second page and confirm it has (exactly) remaining 3 items
-        response = self.client.get(reverse('authors') + '?page=2')
+        response = self.client.get(reverse("authors") + "?page=2")
         self.assertEqual(response.status_code, 200)
-        self.assertTrue('is_paginated' in response.context)
-        self.assertTrue(response.context['is_paginated'] == True)
-        self.assertTrue(len(response.context['author_list']) == 3)
+        self.assertTrue("is_paginated" in response.context)
+        self.assertTrue(response.context["is_paginated"] == True)
+        self.assertTrue(len(response.context["author_list"]) == 3)
 
 
 class RenewBookFormTest(TestCase):
     def test_renew_form_date_field_label(self):
         form = RenewBookModelForm()
         self.assertTrue(
-            form.fields['due_back'].label == None or form.fields['due_back'].label == 'Renewal date')
+            form.fields["due_back"].label == None
+            or form.fields["due_back"].label == "Renewal date"
+        )
 
     def test_renew_form_date_field_help_text(self):
         form = RenewBookModelForm()
-        self.assertEqual(form.fields['due_back'].help_text, 'Enter a date between now and 4 weeks (default 3).')
+        self.assertEqual(
+            form.fields["due_back"].help_text,
+            "Enter a date between now and 4 weeks (default 3).",
+        )
 
     def test_renew_form_date_in_past(self):
         date = datetime.date.today() - datetime.timedelta(days=1)
-        form = RenewBookModelForm(data={'due_back': date})
+        form = RenewBookModelForm(data={"due_back": date})
         self.assertFalse(form.is_valid())
 
     def test_renew_form_date_too_far_in_future(self):
-        date = datetime.date.today() + datetime.timedelta(weeks=4) + datetime.timedelta(days=1)
-        form = RenewBookModelForm(data={'due_back': date})
+        date = (
+            datetime.date.today()
+            + datetime.timedelta(weeks=4)
+            + datetime.timedelta(days=1)
+        )
+        form = RenewBookModelForm(data={"due_back": date})
         self.assertFalse(form.is_valid())
 
     def test_renew_form_date_today(self):
         date = datetime.date.today()
-        form = RenewBookModelForm(data={'due_back': date})
+        form = RenewBookModelForm(data={"due_back": date})
         self.assertTrue(form.is_valid())
 
     def test_renew_form_date_max(self):
         date = timezone.localtime() + datetime.timedelta(weeks=4)
-        form = RenewBookModelForm(data={'due_back': date})
+        form = RenewBookModelForm(data={"due_back": date})
         self.assertTrue(form.is_valid())
--- catalog/views.py
+++ catalog/views.py
@@ -18,19 +18,23 @@ class BookDetailView(generic.DetailView):
 class BookListView(generic.ListView):
     model = Book
     paginate_by = 5
-    context_object_name = 'book_list'  # your own name for the list as a template variable
+    context_object_name = (
+        "book_list"
+    )  # your own name for the list as a template variable
     queryset = Book.objects.all()
-    template_name = '/books/book_list.html'
+    template_name = "/books/book_list.html"
 
 
 class AuthorListView(generic.ListView):
     """Generic class-based list view for a list of authors."""
+
     model = Author
     paginate_by = 10
 
 
 class AuthorDetailView(generic.DetailView):
     """Generic class-based detail view for an author."""
+
     model = Author
 
 
@@ -42,53 +46,59 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
 
     # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
-    num_visits = request.session.get('num_visits', 0)
-    request.session['num_visits'] = num_visits + 1
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
 
     context = {
-        'num_books': num_books,
-        'num_instances': num_instances,
-        'num_instances_available': num_instances_available,
-        'num_authors': num_authors,
-        'num_visits': num_visits,
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+        "num_visits": num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
+    return render(request, "index.html", context=context)
 
 
 class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based view listing books on loan to current user."""
+
     model = BookInstance
-    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    template_name = "catalog/bookinstance_list_borrowed_user.html"
     paginate_by = 10
 
     def get_queryset(self):
-        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+        return (
+            BookInstance.objects.filter(borrower=self.request.user)
+            .filter(status__exact="o")
+            .order_by("due_back")
+        )
 
 
 class LoanedBooksAllListView(PermissionRequiredMixin, generic.ListView):
     """Generic class-based view listing all books on loan. Only visible to users with can_mark_returned permission."""
+
     model = BookInstance
-    permission_required = 'catalog.can_mark_returned'
-    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    permission_required = "catalog.can_mark_returned"
+    template_name = "catalog/bookinstance_list_borrowed_all.html"
     paginate_by = 10
 
     def get_queryset(self):
-        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
+        return BookInstance.objects.filter(status__exact="o").order_by("due_back")
 
 
-@permission_required('catalog.can_mark_returned')
+@permission_required("catalog.can_mark_returned")
 def renew_book_librarian(request, pk):
     book_instance = get_object_or_404(BookInstance, pk=pk)
 
     # If this is a POST request then process the Form data
-    if request.method == 'POST':
+    if request.method == "POST":
 
         # Create a form instance and populate it with data from the request (binding):
         form = RenewBookModelForm(request.POST)
@@ -96,37 +106,51 @@ def renew_book_librarian(request, pk):
         # Check if the form is valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
-            book_instance.due_back = form.cleaned_data['due_back']
+            book_instance.due_back = form.cleaned_data["due_back"]
             book_instance.save()
 
             # redirect to a new URL:
-            return HttpResponseRedirect(reverse('all-borrowed') )
+            return HttpResponseRedirect(reverse("all-borrowed"))
 
     # If this is a GET (or any other method) create the default form.
     else:
         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-        form = RenewBookModelForm(initial={'due_back': proposed_renewal_date})
+        form = RenewBookModelForm(initial={"due_back": proposed_renewal_date})
 
-    context = {
-        'form': form,
-        'book_instance': book_instance,
-    }
+    context = {"form": form, "book_instance": book_instance}
 
-    return render(request, 'catalog/book_renew_librarian.html', context)
+    return render(request, "catalog/book_renew_librarian.html", context)
 
 
 class AuthorCreate(CreateView):
     model = Author
-    fields = '__all__'
-    initial = {'date_of_death': '05/01/2018'}
+    fields = "__all__"
+    initial = {"date_of_death": "05/01/2018"}
 
 
 class AuthorUpdate(UpdateView):
     model = Author
-    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    fields = ["first_name", "last_name", "date_of_birth", "date_of_death"]
 
 
 class AuthorDelete(DeleteView):
     model = Author
-    success_url = reverse_lazy('authors')
+    success_url = reverse_lazy("authors")
 
+
+class BookCreate(PermissionRequiredMixin, CreateView):
+    model = Book
+    fields = "__all__"
+    permission_required = "catalog.can_mark_returned"
+
+
+class BookUpdate(PermissionRequiredMixin, UpdateView):
+    model = Book
+    fields = "__all__"
+    permission_required = "catalog.can_mark_returned"
+
+
+class BookDelete(PermissionRequiredMixin, DeleteView):
+    model = Book
+    success_url = reverse_lazy("books")
+    permission_required = "catalog.can_mark_returned"
